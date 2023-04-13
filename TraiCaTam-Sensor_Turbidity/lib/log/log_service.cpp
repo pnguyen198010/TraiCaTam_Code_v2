ce6f8e8b2477ee88791c13529e85813011aa33df
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-04-13 23:03:09 +0700

    [log] import files

---
+++ TraiCaTam-Sensor_Turbidity/lib/log/log_service.cpp
@@ -0,0 +1,265 @@
+/* ==================================================
+** Libraries
+**
+** =============================================== */
+
+
+#include "log_service.h"
+
+
+/* ==================================================
+** Macro definition
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Type definition
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Extern variables
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Static variables
+**
+** =============================================== */
+
+
+static bool flg_upd = false;
+
+
+/* ==================================================
+** Static function declaration
+**
+** =============================================== */
+
+
+static void myPrintf(const char *format, ...);
+
+
+/* ==================================================
+** Static function definition
+**
+** =============================================== */
+
+
+static void myPrintf(const char *format, ...)
+{
+#ifdef LOG_PORT
+
+    char loc_buf[64];
+    char * temp = loc_buf;
+
+    va_list arg;
+    va_list copy;
+
+    va_start(arg, format);
+    va_copy(copy, arg);
+
+    int len = vsnprintf(temp, sizeof(loc_buf), format, copy);
+    va_end(copy);
+
+    if(len < 0) {
+        va_end(arg);
+        return;
+    };
+
+    if((unsigned long)len >= sizeof(loc_buf))
+    {
+        temp = (char*) malloc(len+1);
+
+        if(temp == NULL) {
+            va_end(arg);
+            return;
+        }
+
+        len = vsnprintf(temp, len+1, format, arg);
+    }
+
+    va_end(arg);
+    LOG_PORT.print(temp);
+
+    if(temp != loc_buf) {free(temp);}
+
+#endif
+}
+
+
+/* ==================================================
+** Log_t: Constructors
+**
+** =============================================== */
+
+
+Log_t::Log_t()
+{
+}
+
+
+Log_t::Log_t(bool flg_disable)
+{
+    this->flg_disable = flg_disable;
+}
+
+
+/* ==================================================
+** Log_t: Methods
+**
+** =============================================== */
+
+
+void Log_t::raw(const char *format)
+{
+#ifdef LOG_PORT
+
+    if(flg_disable) {return;}
+
+    if(flg_upd) {
+        raw("\n");
+        flg_upd = false;
+    }
+
+    LOG_PORT.print(format);
+
+#endif
+}
+
+
+void Log_t::fmt(const char *format, ...)
+{
+#ifdef LOG_PORT
+
+    if(flg_disable) {return;}
+
+    if(flg_upd) {
+        raw("\n");
+        flg_upd = false;
+    }
+
+    va_list args;
+    va_start(args, format);
+
+    myPrintf(format, args);
+
+    va_end(args);
+
+#endif
+}
+
+
+void Log_t::inf(const char *format, ...)
+{
+#ifdef LOG_PORT
+
+    if(flg_disable) {return;}
+
+    if(flg_upd) {
+        LOG_PORT.print("\n");
+        flg_upd = false;
+    }
+
+    va_list args;
+    va_start(args, format);
+
+    myPrintf("I (%lu) ", millis());
+    myPrintf(format, args);
+    LOG_PORT.print("\n");
+
+    va_end(args);
+
+#endif
+}
+
+
+void Log_t::err(const char *format, ...)
+{
+#ifdef LOG_PORT
+
+    if(flg_disable) {return;}
+
+    if(flg_upd) {
+        LOG_PORT.print("\n");
+        flg_upd = false;
+    }
+
+    va_list args;
+    va_start(args, format);
+
+    myPrintf("E (%lu) ", millis());
+    myPrintf(format, args);
+    LOG_PORT.print("\n");
+
+    va_end(args);
+
+#endif
+}
+
+
+void Log_t::upd(const char *format, ...)
+{
+#ifdef LOG_PORT
+
+    if(flg_disable) {return;}
+
+    if(flg_upd == false) {
+        flg_upd = true;
+    }
+
+    va_list args;
+    va_start(args, format);
+
+    myPrintf("U (%lu) ", millis());
+    myPrintf(format, args);
+    LOG_PORT.print("\r");
+
+    va_end(args);
+
+#endif
+}
+
+
+void Log_t::enable()
+{
+    flg_disable = false;
+}
+
+
+void Log_t::disable()
+{
+    flg_disable = true;
+}
+
+
+/* ==================================================
+** Extern function definition
+**
+** =============================================== */
+
+
+void Log_init()
+{
+#ifdef LOG_PORT
+
+    LOG_PORT.begin(LOG_BAUD_RATE);
+    delay(LOG_TIME_INIT);
+
+    LOG_PORT.print(F("\n\n"));
+
+#endif
+}
+
---
+++ TraiCaTam-Sensor_Turbidity/lib/log/log_service.h
@@ -0,0 +1,71 @@
+#ifndef log_h
+#define log_h
+
+
+/* ==================================================
+** Libraries
+**
+** =============================================== */
+
+
+#include <Arduino.h>
+
+
+/* ==================================================
+** Macro definition
+**
+** =============================================== */
+
+
+#define LOG_ENABLE
+
+#ifdef  LOG_ENABLE
+#define LOG_PORT            Serial
+#define LOG_BAUD_RATE       9600
+#define LOG_TIME_INIT       1000
+#endif
+
+
+/* ==================================================
+** Type definition
+**
+** =============================================== */
+
+
+class Log_t
+{
+    public: Log_t();
+    public: Log_t(bool flg_disable);
+
+    public: inline void raw (const char *format);
+    public: inline void fmt (const char *format, ...);
+    public: inline void inf (const char *format, ...);
+    public: inline void err (const char *format, ...);
+    public: inline void upd (const char *format, ...);
+
+    public: void enable();
+    public: void disable();
+
+    private: bool flg_disable;
+};
+
+
+/* ==================================================
+** Extern function declaration
+**
+** =============================================== */
+
+
+void Log_init();
+
+
+/* ==================================================
+** Extern variables
+**
+** =============================================== */
+
+
+//
+
+
+#endif
