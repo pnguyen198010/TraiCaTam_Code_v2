5d2d31d0c6f820c2a318c753ea9b9c6d235cc668
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-04-13 23:41:00 +0700

    [log] Kết hợp với nhánh đã test log service - Bỏ code test khỏi file main

--- TraiCaTam-Sensor_Turbidity/src/main.cpp
+++ TraiCaTam-Sensor_Turbidity/src/main.cpp
@@ -16,11 +16,7 @@
 ** =============================================== */
 
 
-Log_t LOG;
-
-bool flg_log = true;
-
-const uint32_t TIME_TOGGLE = 5000;
+//
 
 
 /* ==================================================
@@ -32,33 +28,10 @@ const uint32_t TIME_TOGGLE = 5000;
 void setup() 
 {
   Log_init();
-
-  LOG.raw("[Log] inited");
-  LOG.fmt("I(%lu) [Log] this is fmt function");
-  LOG.inf("This is info function");
-  LOG.inf("This is err function");
 }
 
 
 void loop() 
 {
-  static uint32_t intv = millis();
-
-  if(millis() - intv < TIME_TOGGLE){
-    LOG.upd("This is log upd");
-    return;
-  }
-
-  if(flg_log){
-    LOG.inf("Disable log service");
-    LOG.disable();
-  }
-
-  else{
-    LOG.enable();
-    LOG.inf("Enable log service");
-  }
-
-  flg_log = !flg_log;
-  intv = millis();
+  
 }
