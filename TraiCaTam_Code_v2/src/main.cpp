3a40c952a983fe08f7e7ca32293a5f38f6f4f12d
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-04-14 10:11:05 +0700

    [log] Sửa lỗi không log được millis() - Không được gọi gián tiếp hàm myPrintf - Không nên sử dụng kiểu uint64_t - Không định dạng được chuỗi có 2 tham số trở lên - Không định dạng được số thực

--- TraiCaTam_Code_v2/src/main.cpp
+++ TraiCaTam_Code_v2/src/main.cpp
@@ -34,9 +34,30 @@ void setup()
   Log_init();
 
   LOG.raw("[Log] inited");
-  LOG.fmt("I(%lu) [Log] this is fmt function");
-  LOG.inf("This is info function");
-  LOG.inf("This is err function");
+
+  LOG.raw("\n\nTest fmt function\n");
+  LOG.fmt("d:  %d,  %d,  %d\n", 1, 2, 3);
+  LOG.fmt("u:  %u,  %u,  %u\n", 1, 2, 3);
+  LOG.fmt("lu: %lu, %lu, %lu\n", (uint64_t)1, (uint64_t)2, (uint64_t)3);
+  LOG.fmt("s:  %s,  %s,  %s\n", "1", "2", "3");
+  LOG.fmt("f:  %f,  %f,  %f\n", 1, 2, 3);
+
+  LOG.raw("\n\nTest inf function\n");
+  LOG.inf("d:  %d,  %d,  %d", 1, 2, 3);
+  LOG.inf("u:  %u,  %u,  %u", 1, 2, 3);
+  LOG.inf("lu: %lu, %lu, %lu", (uint64_t)1, (uint64_t)2, (uint64_t)3);
+  LOG.inf("s:  %s,  %s,  %s", "1", "2", "3");
+  LOG.inf("f:  %f,  %f,  %f", 1, 2, 3);
+
+  LOG.raw("\n\nTest err function\n");
+  LOG.err("d:  %d,  %d,  %d", 1, 2, 3);
+  LOG.err("u:  %u,  %u,  %u", 1, 2, 3);
+  LOG.err("lu: %lu, %lu, %lu", (uint64_t)1, (uint64_t)2, (uint64_t)3);
+  LOG.err("s:  %s,  %s,  %s", "1", "2", "3");
+  LOG.err("f:  %f,  %f,  %f", 1, 2, 3);
+
+  LOG.raw("\n\nTest upd function\n");
+
 }
 
 
@@ -45,7 +66,7 @@ void loop()
   static uint32_t intv = millis();
 
   if(millis() - intv < TIME_TOGGLE){
-    LOG.upd("This is log upd");
+    LOG.upd("%%u: %u | %%lu: %lu | %%lu: %lu | %%f: %f", (uint32_t)10000, (uint64_t)10000, millis(), 10000.00);
     return;
   }
 
