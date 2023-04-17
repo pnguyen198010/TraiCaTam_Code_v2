dca09cccf248080be3cf6bf911a7f87c1138ce09
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-04-17 14:04:22 +0700

    [lora] thay đổi địa chỉ

--- TraiCaTam_Code_v2/lib/lora/lora_address.h
+++ TraiCaTam_Code_v2/lib/lora/lora_address.h
@@ -18,44 +18,44 @@
 
 
 // Address of gateway
-#define ADDL_GATEWAY			0x01
+#define ADDL_GATEWAY			0x00
 #define ADDH_GATEWAY			0x00
-#define CHAN_GATEWAY			0x01
+#define CHAN_GATEWAY			0x00
 
 // Address of sensor 1
 #define ADDL_SENSOR1			0x00
 #define ADDH_SENSOR1			0x00
-#define CHAN_SENSOR1			0x02
+#define CHAN_SENSOR1			0x01
 
 // Address of sensor 2
 #define ADDL_SENSOR2			0x01
 #define ADDH_SENSOR2			0x00
-#define CHAN_SENSOR2			0x02
+#define CHAN_SENSOR2			0x01
 
 // Address of sensor 3
 #define ADDL_SENSOR3			0x02
 #define ADDH_SENSOR3			0x00
-#define CHAN_SENSOR3			0x02
+#define CHAN_SENSOR3			0x01
 
 // Address of large bell 1
 #define ADDL_BELL1_LARGE		0x00
 #define ADDH_BELL1_LARGE		0x00
-#define CHAN_BELL1_LARGE		0x03
+#define CHAN_BELL1_LARGE		0x02
 
 // Address of small bell 1
 #define ADDL_BELL1_SMALL		0x01
 #define ADDH_BELL1_SMALL		0x00
-#define CHAN_BELL1_SMALL		0x03
+#define CHAN_BELL1_SMALL		0x02
 
 // Address of large bell 2
 #define ADDL_BELL2_LARGE		0x00
 #define ADDH_BELL2_LARGE		0x00
-#define CHAN_BELL2_LARGE		0x04
+#define CHAN_BELL2_LARGE		0x03
 
 // Address of small bell 2
 #define ADDL_BELL2_SMALL		0x01
 #define ADDH_BELL2_SMALL		0x00
-#define CHAN_BELL2_SMALL		0x04
+#define CHAN_BELL2_SMALL		0x03
 
 
 /* ==================================================
