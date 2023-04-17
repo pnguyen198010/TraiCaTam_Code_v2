a5e5ae937d8511f4746f03adcc2ac2be51533906
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-04-17 14:11:44 +0700

    [servo] import files

---
+++ TraiCaTam_Code_v2/lib/servo/servo_service.cpp
@@ -0,0 +1,128 @@
+/* ==================================================
+** Libraries
+**
+** =============================================== */
+
+
+#include "servo_service.h"
+
+#include <Servo.h>
+
+#include "turbidity.h"
+
+
+/* ==================================================
+** Macro definition
+**
+** =============================================== */
+
+
+#define SERVO_PIN           9
+
+#define SERVO_NUM_SWEEP     2
+#define SERVO_TIME_PER_POS  15
+
+
+/* ==================================================
+** Type definition
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Extern variables
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Static variables
+**
+** =============================================== */
+
+
+static Servo servo;
+
+static int16_t pos = 0;
+
+// static const float TIME_SWEEP = (float) SERVO_NUM_SWEEP * 360 * SERVO_TIME_PER_POS / 1000;
+static const uint32_t TIME_WAIT_SWEEP = 1 * 10 * 1000;
+
+
+/* ==================================================
+** Static function declaration
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Static function definition
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Extern function definition
+**
+** =============================================== */
+
+
+void Servo_init()
+{
+    servo.attach(SERVO_PIN);
+    servo.write(pos);
+}
+
+
+void Servo_upd_pos()
+{
+    static uint32_t intv = millis();
+
+    if(millis() - intv < TIME_WAIT_SWEEP) {return;}
+
+    Turbidity_disable();
+
+    for(uint8_t i=0; i<SERVO_NUM_SWEEP; ++i)
+    {
+        Serial.print(millis());
+        Serial.print(" [Servo] start ");
+        Serial.println(i);
+
+        for(pos=0; pos<=180; ++pos){
+            servo.write(pos);
+            delay(SERVO_TIME_PER_POS);
+
+            // Serial.print(millis());
+            // Serial.print(" \t pos: ");
+            // Serial.println(pos);
+        }
+
+        for(pos=180; pos>=0; --pos){
+            servo.write(pos);
+            delay(SERVO_TIME_PER_POS);
+
+            // Serial.print(millis());
+            // Serial.print(" \t pos: ");
+            // Serial.println(pos);
+        }
+
+        Serial.print(millis());
+        Serial.print(" Servo] end ");
+        Serial.println(i);
+    }
+
+    Turbidity_enable();
+    intv = millis();
+}
