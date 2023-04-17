d530510d0dfdb50eec18263d18fb3d161100f304
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-04-18 01:43:49 +0700

    [led] điều khiển tháp led 2 màu - Tắt tất cả - Chỉ bật đèn xanh lá - Chỉ bật đèn đỏ

---
+++ TraiCaTam_Code_v2/lib/led/led.cpp
@@ -0,0 +1,99 @@
+/* ==================================================
+** Libraries
+**
+** =============================================== */
+
+
+#include "led.h"
+
+
+/* ==================================================
+** Macro definition
+**
+** =============================================== */
+
+
+#define LED_PIN_GREEN               22
+#define LED_PIN_RED                 23
+
+
+/* ==================================================
+** Type definition
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Extern variables
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Static variables
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Static function declaration
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Static function definition
+**
+** =============================================== */
+
+
+//
+
+
+/* ==================================================
+** Extern function definition
+**
+** =============================================== */
+
+
+void Led_init()
+{
+    pinMode(LED_PIN_GREEN, OUTPUT);
+    pinMode(LED_PIN_RED, OUTPUT);
+
+    digitalWrite(LED_PIN_GREEN, HIGH);
+    digitalWrite(LED_PIN_RED, LOW);
+}
+
+
+void Led_turn_off()
+{
+    digitalWrite(LED_PIN_GREEN, LOW);
+    digitalWrite(LED_PIN_RED,   LOW);
+}
+
+
+void Led_turn_green()
+{
+    digitalWrite(LED_PIN_GREEN, HIGH);
+    digitalWrite(LED_PIN_RED,   LOW);
+}
+
+
+void Led_turn_red()
+{
+    digitalWrite(LED_PIN_GREEN, LOW);
+    digitalWrite(LED_PIN_RED,   HIGH);
+}
