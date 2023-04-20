af078af338d7350cf4db913c384e884a00c43652
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-04-20 13:57:28 +0700

    [led] relay kích mức low, không phải mức high

--- TraiCaTam_Code_v2/lib/led/led.cpp
+++ TraiCaTam_Code_v2/lib/led/led.cpp
@@ -73,27 +73,26 @@ void Led_init()
     pinMode(LED_PIN_GREEN, OUTPUT);
     pinMode(LED_PIN_RED, OUTPUT);
 
-    digitalWrite(LED_PIN_GREEN, HIGH);
-    digitalWrite(LED_PIN_RED, LOW);
+    Led_turn_green();
 }
 
 
 void Led_turn_off()
 {
-    digitalWrite(LED_PIN_GREEN, LOW);
-    digitalWrite(LED_PIN_RED,   LOW);
+    digitalWrite(LED_PIN_GREEN, HIGH);
+    digitalWrite(LED_PIN_RED,   HIGH);
 }
 
 
 void Led_turn_green()
 {
-    digitalWrite(LED_PIN_GREEN, HIGH);
-    digitalWrite(LED_PIN_RED,   LOW);
+    digitalWrite(LED_PIN_GREEN, LOW);
+    digitalWrite(LED_PIN_RED,   HIGH);
 }
 
 
 void Led_turn_red()
 {
-    digitalWrite(LED_PIN_GREEN, LOW);
-    digitalWrite(LED_PIN_RED,   HIGH);
+    digitalWrite(LED_PIN_GREEN, HIGH);
+    digitalWrite(LED_PIN_RED,   LOW);
 }
